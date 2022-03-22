5488c37f9bc2fc601c0428b81f64fad3554d3c2a
malcolmmorton77 <47834908+malcolmmorton77@users.noreply.github.com>
2022-03-22 10:21:50 -0500

    created html for login page (#16)

--- angular-src/tailwind.config.js
+++ angular-src/tailwind.config.js
@@ -1,11 +1,43 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
       "./src/**/*.{html,ts}",
   ],
   theme: {
     colors: {
-      'taupe': '#FFF5DA', 
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'lite-pink': '#F7DAD9',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'bluesign': '#007AFF',
+      blue: colors.blue,
+      yellow: colors.amber,
+      gray: colors.slate,
+      white: colors.white,
     },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Salsa', 'serif'],
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    }, 
     extend: {},
   },
   plugins: [
--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ const username = process.env.DATABASE_USERNAME;
 const password = process.env.DATABASE_PASSWORD;
 
 module.exports = {
-    database: `mongodb+srv://${username}:${password}@cluster0.2s5jl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    // database: 'mongodb://localhost:27017/database',  // for development
-    secret: 'yoursecret'
+    database: `mongodb+srv://${username}:${password}@cluster0.2s5jl.mongodb.net/Cluster0?retryWrites=true&w=majority`,
+    secret: 'yoursecret',
 }
