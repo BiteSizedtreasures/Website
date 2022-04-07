13149500fe69f70fc4959d57f3db054895d4009c
malcolmmorton77 <malcolmmorton77@gmail.com>
2022-04-07 11:46:37 -0500

    pushing changes to deployment

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-// const baseUrl = 'http://localhost:8080/'; // Development
+//const baseUrl = 'http://localhost:8080/'; // Development
 const baseUrl = ''; // Production
 @Injectable({
   providedIn: 'root'
