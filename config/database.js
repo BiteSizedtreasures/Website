59e4c532b4637cf605d642b0871916fbaef517da
malcolmmorton77 <47834908+malcolmmorton77@users.noreply.github.com>
2022-04-06 21:53:55 -0500

    created html for login page (#23)
    jesus...

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ const username = process.env.DATABASE_USERNAME;
 const password = process.env.DATABASE_PASSWORD;
 
 module.exports = {
+    database: `mongodb+srv://${username}:${password}@cluster0.2s5jl.mongodb.net/Cluster0?retryWrites=true&w=majority`,
     database: `mongodb+srv://${username}:${password}@cluster0.2s5jl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     secret: 'yoursecret',
 }
